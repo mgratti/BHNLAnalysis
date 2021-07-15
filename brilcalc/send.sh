79188753003f6412e4283d86a4d766883e74e5b4
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2021-07-15 18:57:31 +0200

    script to launch all analyses

---
+++ brilcalc/send.sh
@@ -0,0 +1,9 @@
+python analyseBril.py --whichana lumiEach
+python analyseBril.py --whichana lumiTot
+python analyseBril.py --part part0 --whichana lumiTotByPart
+python analyseBril.py --part part1 --whichana lumiTotByPart 
+python analyseBril.py --part part2 --whichana lumiTotByPart 
+python analyseBril.py --part part3 --whichana lumiTotByPart 
+python analyseBril.py --part part4 --whichana lumiTotByPart 
+python analyseBril.py --part part5 --whichana lumiTotByPart 
+python analyseBris.py --whichana lumiOverlap
