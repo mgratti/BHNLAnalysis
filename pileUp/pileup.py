684299c8ed754717fcc4b0d6194842b31f39d7bb
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2021-07-15 19:01:15 +0200

    updated script that performs mock PU reweighting with several distributions

--- pileUp/pileup.py
+++ pileUp/pileup.py
@@ -22,7 +22,7 @@ rdst_pu = [1.286e-05,4.360e-05,1.258e-04,2.721e-04,4.548e-04,7.077e-04,1.074e-03
 bins = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99]
 pu_rdst_mc = TH1F('pu_rdst_mc', 'MC, like R(D*), mixing', 100, 0, 100)
 for i,pu in enumerate(rdst_pu):
-        pu_rdst_mc.SetBinContent(i+1,pu)
+	pu_rdst_mc.SetBinContent(i+1,pu)
 
 # Profiles from files
 _file0 = TFile.Open("MC_PileUp_2018_Autumn18.root")
@@ -67,7 +67,7 @@ pu_bph_data.SetLineWidth(1)
 pu_bph_data_gaus.SetLineColor(kGreen)
 
 for i in range(1,pu_bph_data.GetNbinsX()+1):
-        print(i,pu_bph_data.GetBinContent(i),pu_bph_data_gaus.GetBinContent(i))
+	print(i,pu_bph_data.GetBinContent(i),pu_bph_data_gaus.GetBinContent(i))
 
 pu_bph_mc.DrawNormalized('hist')
 pu_std_mc.DrawNormalized('samehist')
@@ -84,22 +84,22 @@ c.SaveAs('pu_comparison.root')
 
 
 if start=='poisson':
-        pu_start = pu_bph_mc
+	pu_start = pu_bph_mc
 elif start=='std2018-like':
-        pu_start = pu_std_mc_gaus
+	pu_start = pu_std_mc_gaus
 #elif start=='std2018':
-#       pu_start = pu_std_mc
+#	pu_start = pu_std_mc
 elif start=='data-like':
-        pu_start = pu_bph_data_gaus
+	pu_start = pu_bph_data_gaus
 else:
-        raise RuntimeError('not supported')
+	raise RuntimeError('not supported')
 
 if target=='data':
-        pu_target = pu_bph_data
+	pu_target = pu_bph_data
 elif target=='data-like':
-        pu_target = pu_bph_data_gaus
+	pu_target = pu_bph_data_gaus
 else:
-        raise RuntimeError('not supported')
+	raise RuntimeError('not supported')
 
 # Mock the PU reweighting procedure
 # let's assume to have generated the sample with a starting profile pu_start and want to reweight to the PU distribution to pu_target
@@ -131,113 +131,112 @@ biases_cropped = []
 
 for iExp in range(nExp):
 
-        # calculate distribution of the weights in a mock sample
-        c3 = TCanvas()
-        # generate a poissonian distribution with 10K events, determine the weights for those
+	# calculate distribution of the weights in a mock sample
+	c3 = TCanvas()
+	# generate a poissonian distribution with 10K events, determine the weights for those
 
-        weights = TH1F('weights','weights', 100,0,100000)
-        if   start=='std2018' or start=='std2018-like' or start=='data-like':
-                bins = list(np.logspace(np.log10(0.0000001),np.log10(1000),60,base=10))
-        elif start=='poisson':
-                bins = list(np.logspace(np.log10(0.01),np.log10(100000),60,base=10))
-        else:
-                raise RuntimeError()
-        weights_small = TH1F('weights_small', 'weights_small', len(bins)-1, array.array('f',bins))
+	weights = TH1F('weights','weights', 100,0,100000)
+	if   start=='std2018' or start=='std2018-like' or start=='data-like':
+		bins = list(np.logspace(np.log10(0.0000001),np.log10(1000),60,base=10))
+	elif start=='poisson':
+		bins = list(np.logspace(np.log10(0.01),np.log10(100000),60,base=10))
+	else:
+		raise RuntimeError()
+	weights_small = TH1F('weights_small', 'weights_small', len(bins)-1, array.array('f',bins))
 
 
-        Lxy = TH1F('Lxy', 'Lxy (mock)', 25,0.,50.)
-        Lxy_weighted = TH1F('Lxy_weighted', 'Lxy (mock) w/ PU weighting', 25,0.,50.)
-        Lxy_weightedCropped = TH1F('Lxy_weighted', 'Lxy (mock) w/ PU weighting, w/o weights>10 ', 25,0.,50.)
-        #c3.SetLogy()
-        #weights.GetYaxis().SetRangeUser(0.01,100)
+	Lxy = TH1F('Lxy', 'Lxy (mock)', 25,0.,50.)
+	Lxy_weighted = TH1F('Lxy_weighted', 'Lxy (mock) w/ PU weighting', 25,0.,50.)
+	Lxy_weightedCropped = TH1F('Lxy_weighted', 'Lxy (mock) w/ PU weighting, w/o weights>10 ', 25,0.,50.)
+	#c3.SetLogy()
+	#weights.GetYaxis().SetRangeUser(0.01,100)
 
-        if start=='poisson':
-                custom_sample = list(np.random.poisson(20,10000))
-        elif start=='std2018-like':
-                custom_sample = list(np.random.normal(loc=35,scale=11,size=10000))
-        elif start=='data-like':
-                custom_sample = list(np.random.normal(loc=29,scale=8,size=10000))
-        else:
-                raise RuntimeError()
+	if start=='poisson':
+		custom_sample = list(np.random.poisson(20,10000))
+	elif start=='std2018-like':
+		custom_sample = list(np.random.normal(loc=35,scale=11,size=10000))
+	elif start=='data-like':
+		custom_sample = list(np.random.normal(loc=29,scale=8,size=10000))
+	else:
+		raise RuntimeError()
 
-        exp = list(np.random.exponential(scale=20., size=10000)) # exp(-ct/ctau), ctau=20cm., assuming betagamma=1
+	exp = list(np.random.exponential(scale=20., size=10000)) # exp(-ct/ctau), ctau=20cm., assuming betagamma=1
 
-        all_weights = []
+	all_weights = []
 
-        for i,ip in enumerate(custom_sample):
-                bin = max(1, min(ratio.GetNbinsX(), ratio.GetXaxis().FindBin(ip)))
-                weight = ratio.GetBinContent(bin)
-                all_weights.append(weight)
-                weights.Fill(weight)
-                weights_small.Fill(weight)
-                Lxy.Fill(exp[i])
-                Lxy_weighted.Fill(exp[i],weight)
-                weight_cropped = min(weight,10)
-                Lxy_weightedCropped.Fill(exp[i],weight_cropped)
+	for i,ip in enumerate(custom_sample):
+		bin = max(1, min(ratio.GetNbinsX(), ratio.GetXaxis().FindBin(ip)))
+		weight = ratio.GetBinContent(bin)
+		all_weights.append(weight)
+		weights.Fill(weight)
+		weights_small.Fill(weight)
+		Lxy.Fill(exp[i])
+		Lxy_weighted.Fill(exp[i],weight)
+		weight_cropped = min(weight,10)
+		Lxy_weightedCropped.Fill(exp[i],weight_cropped)
 
-        #weights.Draw('hist')
-        c3.SetLogx()
-        c3.SetLogy()
+	#weights.Draw('hist')
+	c3.SetLogx()
+	c3.SetLogy()
 
-        weights_small.DrawNormalized('hist')
-        weights_small.GetXaxis().SetTitle('weight')
-        #weights_small.GetXaxis().SetRangeUser(0.001,100)
-        weights_small.GetYaxis().SetTitle('Entries')
-        weights_small.DrawNormalized('hist')
+	weights_small.DrawNormalized('hist')
+	weights_small.GetXaxis().SetTitle('weight')
+	#weights_small.GetXaxis().SetRangeUser(0.001,100)
+	weights_small.GetYaxis().SetTitle('Entries')
+	weights_small.DrawNormalized('hist')
 
-        label1 = TLatex(0.6, 0.6, 'Mean:{:.2f}\pm{:.2f}, #sigma={:.2f}\pm{:.2f}'.format(weights_small.GetMean(),weights_small.GetMeanError(),weights_small.GetStdDev(),weights_small.GetStdDevError()))
-        label1.SetNDC()
-        label1.SetTextSize(0.03)
-        label1.Draw()
+	label1 = TLatex(0.6, 0.6, 'Mean:{:.2f}\pm{:.2f}, #sigma={:.2f}\pm{:.2f}'.format(weights_small.GetMean(),weights_small.GetMeanError(),weights_small.GetStdDev(),weights_small.GetStdDevError()))
+	label1.SetNDC()
+	label1.SetTextSize(0.03)
+	label1.Draw()
 
-        print 'mean={:.2f}, sigma={:.2f}'.format( weights.GetMean(),       np.sqrt(weights.GetRMS()))
-        print 'mean={:.2f}, sigma={:.2f}'.format( weights_small.GetMean(), np.sqrt(weights_small.GetRMS()))
-        #print all_weights
+	print 'mean={:.2f}, sigma={:.2f}'.format( weights.GetMean(),       np.sqrt(weights.GetRMS()))
+	print 'mean={:.2f}, sigma={:.2f}'.format( weights_small.GetMean(), np.sqrt(weights_small.GetRMS()))
+	#print all_weights
 
-        c3.SaveAs('weights.pdf')
+	c3.SaveAs('weights.pdf')
 
-        # compare mock distributions before and after reweighting
-        c4 = TCanvas()
-        Lxy.SetLineColor(kBlue)
-        Lxy_weighted.SetLineColor(kRed)
-        Lxy_weightedCropped.SetLineColor(kOrange)
-        Lxy_weighted.GetXaxis().SetTitle('mock L_{xy} (cm)')
-        Lxy_weighted.Draw("histPE")
-        Lxy.Draw("histPEsame")
-        Lxy_weightedCropped.Draw("histPEsame")
+	# compare mock distributions before and after reweighting
+	c4 = TCanvas()
+	Lxy.SetLineColor(kBlue)
+	Lxy_weighted.SetLineColor(kRed)
+	Lxy_weightedCropped.SetLineColor(kOrange)
+	Lxy_weighted.GetXaxis().SetTitle('mock L_{xy} (cm)')
+	Lxy_weighted.Draw("histPE")
+	Lxy.Draw("histPEsame")
+	Lxy_weightedCropped.Draw("histPEsame")
 
-        err_Lxy = Double(0)
-        err_Lxy_weighted = Double(0)
-        err_Lxy_weightedCropped = Double(0)
-        yield_Lxy = Lxy.IntegralAndError(0, Lxy.GetNbinsX()+2, err_Lxy)
-        yield_Lxy_weighted = Lxy_weighted.IntegralAndError(0, Lxy_weighted.GetNbinsX()+2, err_Lxy_weighted)
-        yield_Lxy_weightedCropped = Lxy_weightedCropped.IntegralAndError(0, Lxy_weightedCropped.GetNbinsX()+2, err_Lxy_weightedCropped)
-        rel_err_Lxy = err_Lxy/yield_Lxy
-        rel_err_Lxy_weighted = err_Lxy_weighted /yield_Lxy_weighted
-        rel_err_Lxy_weightedCropped = err_Lxy_weightedCropped / yield_Lxy_weightedCropped
+	err_Lxy = Double(0)
+	err_Lxy_weighted = Double(0)
+	err_Lxy_weightedCropped = Double(0)
+	yield_Lxy = Lxy.IntegralAndError(0, Lxy.GetNbinsX()+2, err_Lxy)
+	yield_Lxy_weighted = Lxy_weighted.IntegralAndError(0, Lxy_weighted.GetNbinsX()+2, err_Lxy_weighted)
+	yield_Lxy_weightedCropped = Lxy_weightedCropped.IntegralAndError(0, Lxy_weightedCropped.GetNbinsX()+2, err_Lxy_weightedCropped)
+	rel_err_Lxy = err_Lxy/yield_Lxy
+	rel_err_Lxy_weighted = err_Lxy_weighted /yield_Lxy_weighted
+	rel_err_Lxy_weightedCropped = err_Lxy_weightedCropped / yield_Lxy_weightedCropped
 
-        errs.append(rel_err_Lxy_weighted)
-        errs_cropped.append(rel_err_Lxy_weightedCropped)
-        biases.append(yield_Lxy_weighted/yield_Lxy-1)
-        biases_cropped.append(yield_Lxy_weightedCropped/yield_Lxy-1)
+	errs.append(rel_err_Lxy_weighted)
+	errs_cropped.append(rel_err_Lxy_weightedCropped)
+	biases.append(yield_Lxy_weighted/yield_Lxy-1)
+	biases_cropped.append(yield_Lxy_weightedCropped/yield_Lxy-1)
 
-        yield_label = 'Yield ={:.0f}, +PUrw={:.0f}, +cropping={:.0f}'.format(yield_Lxy,yield_Lxy_weighted,yield_Lxy_weightedCropped)
-        err_label   = 'RelErr={:.3f}, +PUrw={:.3f}, +cropping={:.3f}'.format(rel_err_Lxy,rel_err_Lxy_weighted,rel_err_Lxy_weightedCropped)
-        print yield_label
-        print err_label
-        label2 = TLatex(0.5, 0.5, yield_label)
-        label3 = TLatex(0.5, 0.42, err_label)
-        label2.SetNDC()
-        label2.SetTextSize(0.03)
-        label3.SetNDC()
-        label3.SetTextSize(0.03)
-        c4.BuildLegend(0.6,0.6,0.9,0.9)
-        label3.Draw()
-        label2.Draw()
+	yield_label = 'Yield ={:.0f}, +PUrw={:.0f}, +cropping={:.0f}'.format(yield_Lxy,yield_Lxy_weighted,yield_Lxy_weightedCropped)
+	err_label   = 'RelErr={:.3f}, +PUrw={:.3f}, +cropping={:.3f}'.format(rel_err_Lxy,rel_err_Lxy_weighted,rel_err_Lxy_weightedCropped)
+	print yield_label
+	print err_label
+	label2 = TLatex(0.5, 0.5, yield_label)
+	label3 = TLatex(0.5, 0.42, err_label)
+	label2.SetNDC()
+	label2.SetTextSize(0.03)
+	label3.SetNDC()
+	label3.SetTextSize(0.03)
+	c4.BuildLegend(0.6,0.6,0.9,0.9)
+	label3.Draw()
+	label2.Draw()
 
-        c4.SaveAs('mockLxy_weighted.pdf')
+	c4.SaveAs('mockLxy_weighted.pdf')
 
 
 print 'Avg stat over N={} simulations, err={:.3f} bias={:.3f}'.format(nExp,sum(errs)/nExp,sum(biases)/nExp)
 print 'Avg stat over N={} simulations, err={:.3f} bias={:.3f} cropped'.format(nExp, sum(errs_cropped)/nExp, sum(biases_cropped)/nExp)
-
