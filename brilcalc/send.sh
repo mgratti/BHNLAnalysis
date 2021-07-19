4571b0b7d28095a7ee951f4b69288ff4ac26707c
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2021-07-19 10:28:58 +0200

    minor

--- brilcalc/send.sh
+++ brilcalc/send.sh
@@ -6,4 +6,4 @@ python analyseBril.py --part part2 --whichana lumiTotByPart
 python analyseBril.py --part part3 --whichana lumiTotByPart 
 python analyseBril.py --part part4 --whichana lumiTotByPart 
 python analyseBril.py --part part5 --whichana lumiTotByPart 
-python analyseBris.py --whichana lumiOverlap
+python analyseBril.py --whichana lumiOverlap
